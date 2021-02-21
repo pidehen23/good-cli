ba03e3596c9defe9e4bca77888606ad631a7ce36
陈家敬 <16602132375@163.com>
2021-02-21 22:27:38 +0800

    feat: 🎸 master
    增加react-template-mobile

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,25 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
-}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "type-enum": [
+      2,
+      "always",
+      // 比默认值多了个 deps
+      [
+        "build",
+        "ci",
+        "chore",
+        "deps",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test"
+      ]
+    ]
+  }
+};
--- config/repo.js
+++ config/repo.js
@@ -1,5 +1,6 @@
 const repo = {
   "react-template-pc": "https://github.com/chenjiajing23/react-template-pc.git",
+  "react-template-mobile": "https://github.com/chenjiajing23/react-template-mobile.git",
   "vue-template-pc-3.0": "https://github.com/chenjiajing23/vue-template-pc-3.0.git",
   "vue-template-pc-2.0": "https://github.com/chenjiajing23/vue-template-pc-2.0.git"
 };
